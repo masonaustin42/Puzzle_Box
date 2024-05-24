c4b8bdc075a97a79e085680f4df908e6d3fac42b
masonaustin42 <masonaustin42@gmail.com>
2024-05-23 23:09:59 -0700

    add dependencies

--- puzzle_box.ino
+++ puzzle_box.ino
@@ -1,3 +1,9 @@
+#include <SPI.h>               // include SPI bus library
+#include <MFRC522.h>           // include RFID reader library
+#include <FastLED.h>           // include FastLED Library
+#include <Adafruit_LIS3MDL.h>  // include Adafruit Magnetometer library
+#include 
+
 void setup() {
   // put your setup code here, to run once:
 
