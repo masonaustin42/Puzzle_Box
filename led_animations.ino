753bc2389e787c09c90ed6e12647260cd0e69061
masonaustin42 <masonaustin42@gmail.com>
2024-07-15 12:05:39 -0700

    remove unnecessary files and finalize code

--- led_animations.ino
+++ led_animations.ino
@@ -9,26 +9,30 @@ void ledPulsing() {
       leds[j] = CHSV(PointHues[objective], 200, 255);
     }
   }
-  fadeToBlackBy(leds, NUM_LEDS, 4);
+  fadeToBlackBy(leds, NUM_LEDS, 10);
 }
 
-void ledSearching(bool spiral) {
-  uint8_t saturation = beat8(30, 0);
-  uint8_t ring1 = map(beat8(30,600 * spiral), 0, 255, Rings[0][0], Rings[0][1]);
-  uint8_t ring2 = map(beat8(30,450 * spiral), 0, 255, Rings[1][0], Rings[1][1]);
-  uint8_t ring3 = map(beat8(30,300 * spiral), 0, 255, Rings[2][0], Rings[2][1]);
-  uint8_t ring4 = map(beat8(30,150 * spiral), 0, 255, Rings[3][0], Rings[3][1]);
-  uint8_t ring5 = map(beat8(30,0), 0, 255, Rings[4][0], Rings[4][1]);
 
-  leds[ring1] = CHSV(85, saturation, 255);
-  leds[ring2] = CHSV(85, saturation, 255);
-  leds[ring3] = CHSV(85, saturation, 255);
-  leds[ring4] = CHSV(85, saturation, 255);
-  leds[ring5] = CHSV(85, saturation, 255);
-  leds[92] = CHSV(85, saturation, 255);
+
+void ledSearching() {
+  uint8_t speed = beat8(30, 0);
+  uint8_t ring1 = map(speed, 0, 255, Rings[0][0], Rings[0][1]);
+  uint8_t ring2 = map(speed, 0, 255, Rings[1][0], Rings[1][1]);
+  uint8_t ring3 = map(speed, 0, 255, Rings[2][0], Rings[2][1]);
+  uint8_t ring4 = map(speed, 0, 255, Rings[3][0], Rings[3][1]);
+  uint8_t ring5 = map(speed, 0, 255, Rings[4][0], Rings[4][1]);
+
+  leds[ring1] = CHSV(85, speed, 255);
+  leds[ring2] = CHSV(85, speed, 255);
+  leds[ring3] = CHSV(85, speed, 255);
+  leds[ring4] = CHSV(85, speed, 255);
+  leds[ring5] = CHSV(85, speed, 255);
+  leds[92] = CHSV(85, speed, 255);
 
   fadeToBlackBy(leds, NUM_LEDS, 3);
 }
+
+ 
 
 void ledPoints() {
   FastLED.clear();
@@ -54,4 +58,14 @@ void ledPoints() {
     int pointLed = ((bearing / 360) * RingSizes[selectedRing]) + offset;
     leds[pointLed].setHue(PointHues[objective]);
     leds[92].setHue(hue);
+}
+
+void ledRainbow() {
+  for (int ring = 0; ring <= 4; ring++) {
+    for (int i = Rings[ring][0]; i <= Rings[ring][1]; i++) {
+      uint8_t wave = (255 / (Rings[ring][1] - Rings[ring][0] + 1)) * (Rings[ring][1] - i);
+      leds[i].setHue(hue + wave);
+    }
+  }
+  leds[92].setHue(hue);
 }
--- puzzle_box.ino
+++ puzzle_box.ino
@@ -1,7 +1,8 @@
-#include <WS2812Serial.h>      // include library to use serial port on Teensy for neopixels
+#include <WS2812Serial.h>
 #define USE_WS2812SERIAL
 #include <FastLED.h>           // include FastLED Library
 #include <TinyGPSPlus.h>       // include GPS Data parsing library
+// #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <math.h>
 
@@ -12,47 +13,42 @@
 
 // Define GPS Points
 const double Points[][2] = {
-                            // { 47.542046203062654, -122.54614416764574},  // hal's parents
-                            // { 47.46740713249172, -122.61465463015585},   // home
-                            { 47.5424255792486, -122.6373545951069 },    // Merc
-                            { 47.53358012994527, -122.60760145751593}, // Taco Bell
-                            { 47.5166091859565, -122.62829766716061}, // Walmart
-                            { 47.50396635151055, -122.65078295913231}, // SC
-                            // { 47.55570665517761, -122.54354641428509},   //manchester
-                            // { 47.56710558284195, -122.64955548857306},   //insight
-                            // { 47.66473591230466, -122.69514208676632},   //trappers
-                            // { 47.476690187351664, -122.5491264948875},   //banner forest
-                            // { 47.422176222346536, -122.54161606046057 }, // OBM
-                            // { 47.43409098724879, -122.54497605607479},   // Mom & Dad's
+                            // { 47.542046203062654, -122.54614416764574} // hal's parents
+                            // { 47.46740713249172, -122.61465463015585}  // home
+                            // { 47.5424255792486, -122.6373545951069 },  // Merc
+                            // { 47.53358012994527, -122.60760145751593}, // Taco Bell
+                            // { 47.5166091859565, -122.62829766716061},  // Walmart
+                            // { 47.50396635151055, -122.65078295913231}, // SC
+                            { 47.50369865428797, -122.62911951854471},    // Fred Meyers
+                            { 47.55570665517761, -122.54354641428509},    //manchester
+                            { 47.565382834821264, -122.64965353307728},    //insight
+                            { 47.66473591230466, -122.69514208676632},    //trappers
+                            { 47.476690187351664, -122.5491264948875},    //banner forest
+                            { 47.422176222346536, -122.54161606046057 },  // OBM
+                            { 47.43409098724879, -122.54497605607479},    // Mom & Dad's
                             }; 
-const int numPoints = 4;
+const int numPoints = 7;
 int PointHues[] = {192, 64, 96, 160, 32, 210, 0};
 uint8_t hue = 0;
 int objective = 0;
 int timeAtObjective = 0;
+bool timeIsCounting = false;
+
+const int objectiveTimeout = 180;
 
 const int GPS_Baud = 9600;
 // Instantiate TinyGPS+ object
 TinyGPSPlus gps;
 
-bool GPSConnected = false;
+#define GPS Serial5
 
 double bearing = 0;
 double distance = 100.0;
 
-// Instantiate DFPlayer trigger
-// DFPlayerMini_Fast MP3;
-const uint8_t MP3_PIN_1 = 2;
-const uint8_t MP3_PIN_2 = 3;
-unsigned long MP3_1_Millis = 0;
-unsigned long MP3_2_Millis = 0;
-
-unsigned long currentMillis = 0;
-
 
 // Instantiate LED Rings
 const int NUM_LEDS = 93;
-const int LED_PIN = 1;
+const int LED_PIN = 35;
 
 CRGB leds[NUM_LEDS];
 
@@ -77,23 +73,18 @@ void setup() {
   Serial.begin(9600);
   #endif
 
-  pinMode(MP3_PIN_1, OUTPUT);
-  pinMode(MP3_PIN_2, OUTPUT);
-
-  Serial7.begin(GPS_Baud);
+  GPS.begin(GPS_Baud);
 
   FastLED.addLeds<WS2812SERIAL, LED_PIN, BRG>(leds, NUM_LEDS);
-  FastLED.setBrightness(10);
+  FastLED.setBrightness(75);
+  // FastLED.setDither(0);
 
 }
 
 void loop() {
-  // update currentMillis value
-  currentMillis = millis();
-
   // read GPS data
-  while (Serial7.available() > 0) {
-    gps.encode(Serial7.read());
+  while (GPS.available() > 0) {
+    gps.encode(GPS.read());
   }
   // if received valid location data from GPS
   if (gps.location.isValid()) {
@@ -109,29 +100,28 @@ void loop() {
   // Calculate bearings of each Objective
   bearing = calculateBearing(gps.location.lat(), gps.location.lng(), Points[objective][0], Points[objective][1]);
 
-
-  // draw the map
-  drawMap(distance, bearing);
-
   // update global hue value 
-  EVERY_N_MILLISECONDS(10) {
-    hue++;
+  EVERY_N_MILLISECONDS(75) {
+    hue += 10;
   }
 
-  // check how long we've been at currently selected obj to select next one after 300 seconds
+  // check how long we've been at currently selected obj to select next one after 180 seconds
   EVERY_N_SECONDS(1) {
-    // debugPrint("Time at current Objective: ");
     debugPrint("Time at current Objective: ");
     // debugPrintln(timeAtObjective);
-    debugPrintln(timeAtObjective);
-    if (timeAtObjective > 0 && timeAtObjective < 180) {
+    if (timeAtObjective > 0 && timeAtObjective < objectiveTimeout) {
       timeAtObjective += 1;
-    } else if (timeAtObjective >= 180) {
-      timeAtObjective = 0;
+    } else if (timeAtObjective == objectiveTimeout) {
+      timeAtObjective += 1;
       objective += 1;
+      timeIsCounting = false;
+    } else {
+      timeAtObjective = 0;
     }
   }
 
+  // Draw the map
+  drawMap(distance, bearing);
 }
 
 // Function to draw the map based off of:
@@ -141,30 +131,36 @@ void drawMap(double distance, double bearing) {
   #ifdef DEBUG
   displayGPSInfo();
   #endif
-  
-  debugPrint("Objective Status: ");
-  if (! gps.location.isValid()) {
-    debugPrintln("GPS Searching...");
-    ledSearching(false);
-  } else if (distance < 0.1) {
-    debugPrintln("Close to point");
-    ledPulsing();
-    // set timeAtObjective to 1 once, only when arriving at objective
+
+    debugPrint("objective num: ");
+    debugPrintln(objective);
+    debugPrint("Time at Objective: ");
     debugPrintln(timeAtObjective);
-    if (timeAtObjective < 1 || timeAtObjective > 300) {
-      timeAtObjective = 1;
-      // play objective found mp3
-      digitalWrite(MP3_PIN_2, LOW);
-      };
+  if (objective >= numPoints) {
+    ledRainbow();
   } else {
-    debugPrintln("Displaying points...");
-    ledPoints();
-    debugPrint("Bearing: ");
-    debugPrintln(bearing);
-    debugPrint("Distance: ");
-    debugPrintln(distance);
-    // play next point mp3
-      digitalWrite(MP3_PIN_1, LOW);
+    debugPrint("Objective Status: ");
+    if (! gps.location.isValid()) {
+      debugPrint("GPS Searching    ");
+      ledSearching();
+    } else if (distance < 0.1) {
+      debugPrint("Close to point    ");
+      ledPulsing();
+      // set timeIsCounting to true when entering the space
+      // debugPrintln(timeAtObjective);
+      if (!timeIsCounting && timeAtObjective == 0) {
+        timeIsCounting = true;
+        timeAtObjective = 1;
+        };
+    } else {
+      debugPrint("Displaying points    ");
+      ledPoints();
+      debugPrint("Bearing: ");
+      // debugPrintln(bearing);
+      debugPrint("Distance: ");
+      // debugPrintln(distance);
+    }
+    debugPrintln("");
   }
   
   FastLED.show();
@@ -200,27 +196,4 @@ double calculateBearing(double lat1, double lon1, double lat2, double lon2) {
   bearing = fmod((bearing + 360.0), 360.0);
 
   return bearing;
-}
-
-void updateMP3Pins(uint8_t pin) {
-  // Checks to set MP3 pins back to high if they have been low for 200 milliseconds
-  // Check pin #1 first
-  if (MP3_PIN_1 == LOW) {
-    // if it has been 200 millis since mp3 #1 was set high
-    if (currentMillis - MP3_1_Millis >= 200) {
-      digitalWrite(MP3_PIN_1, HIGH);
-    }
-  } else if (MP3_PIN_1 == HIGH) {
-    // update mp3 #1 value to current time
-    MP3_1_Millis = currentMillis;
-  }
-
-  // Same check for pin #2
-  if (MP3_PIN_2 == LOW) {
-    if (currentMillis - MP3_2_Millis >= 200) {
-      digitalWrite(MP3_PIN_2, HIGH);
-    }
-  } else if (MP3_PIN_2 == HIGH) {
-    MP3_2_Millis = currentMillis;
-  }
 }
